d8c34d2b93d65a745c143306eeec9d1ba89efbc4
JoshRichmond06 <joshua.richmond153@gmail.com>
2024-01-22 20:15:54 -0600

    Add Circle

---
+++ circle.js
@@ -0,0 +1,6 @@
+export class Ccircle {
+    constructor(pos, r) {
+        this.position = pos;
+        this.radius = r;
+    }
+}
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import { Renderer } from './renderer.js';
+import { Circle } from './circle.js';
 import { Rectangle } from './rectangle.js';
+import { Renderer } from './renderer.js';
+
 
 const canv = document.getElementById("canvas"); //find canvas elements on  webpage reference html doc, element name is one of the things in html, in this case canvas
 const ctx = canv.getContext("2d");  //used for drawing shapes on canvas, ctx has all the methods for drawing
