2685aea0f9b237eb7828ee66bb7b4ab02d490728
JoshRichmond06 <joshua.richmond153@gmail.com>
2024-02-20 11:19:47 -0600

    add shape class

--- circle.js
+++ circle.js
@@ -1,16 +1,16 @@
 export class Circle {
-	constructor(pos, r) {
-		this.position = pos
-		this.radius = r;
-	}
+    constructor(pos, r) {
+        this.position = pos
+        this.radius = r;
+    }
 
-	draw(ctx, strokeColor, fillColor) {
+    draw(ctx, strokeColor, fillColor) {
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, true);
         ctx.closePath();
         if (fillColor) {
             ctx.fillStyle = fillColor;
-            ctx.fill();
+        ctx.fill();
         }
         ctx.strokeStyle = strokeColor;
         ctx.lineWidth = 3;
